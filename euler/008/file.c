a9e8a95c66569bfd4ad514effb67313afaf2b82d
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-05-18 03:44:07 +0300

    Used long instead of int, still not working

--- euler/008/file.c
+++ euler/008/file.c
@@ -5,9 +5,9 @@
 #define LINE_LENGTH 50
 #define LINES 20
 
-static int getProduct( char *line, int count )
+static long getProduct( char *line, int count )
 {
-    int product = 1;
+    long product = 1;
     for ( int i = 0; i < count; i++ ) {
         product *= line[ i ] - '0';
     }
@@ -15,12 +15,12 @@ static int getProduct( char *line, int count )
     return product;
 }
 
-static int largestProduct( char *line, int count )
+static long largestProduct( char *line, int count )
 {
-    int largest = getProduct( line++, count );
+    long largest = getProduct( line++, count );
 
     while( line[ count - 1 ] ) {
-        int current = getProduct( ++line, count );
+        long current = getProduct( ++line, count );
 
         if ( current > largest ) {
             largest = current;
@@ -33,18 +33,18 @@ static int largestProduct( char *line, int count )
 int main()
 {
     FILE *input = fopen( "input.txt", "r" );
-    int count = 4;
+    int count = 13;
 
-    int largest = 0;
+    long largest = 0;
     char line[ LINE_LENGTH + 1 ];
     while ( fscanf( input, "%s", line ) == 1 ) {
-        int current = largestProduct( line, count );
+        long current = largestProduct( line, count );
         if ( current > largest ) {
             largest = current;
         }
     }
 
-    printf( "Largest product of %d\nadjacesnt numbers: %d\n", count, largest );
+    printf( "Largest product of %d\nadjacesnt numbers: %ld\n", count, largest );
 
     fclose( input );
     return EXIT_SUCCESS;
