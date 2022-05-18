2d3723fde88b2a5b7c1b2b51cda28493631becfe
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-05-18 03:39:25 +0300

    Works for 4

--- euler/008/file.c
+++ euler/008/file.c
@@ -5,21 +5,22 @@
 #define LINE_LENGTH 50
 #define LINES 20
 
-static int largestProduct( char *line, int count )
+static int getProduct( char *line, int count )
 {
-    int index = 0;
-    int largest = 1;
-
-    // Multiply first count digits
+    int product = 1;
     for ( int i = 0; i < count; i++ ) {
-        largest *= line[ index++ ] - '0';
+        product *= line[ i ] - '0';
     }
 
-    // Get new ones as you go, dropping first, adding last
-    int current = largest;
-    while ( line[ index ] ) {
-        current /= line[ index - count ] - '0';
-        current *= line[ index++ ] - '0';
+    return product;
+}
+
+static int largestProduct( char *line, int count )
+{
+    int largest = getProduct( line++, count );
+
+    while( line[ count - 1 ] ) {
+        int current = getProduct( ++line, count );
 
         if ( current > largest ) {
             largest = current;
@@ -34,7 +35,7 @@ int main()
     FILE *input = fopen( "input.txt", "r" );
     int count = 4;
 
-    int largest = -1;
+    int largest = 0;
     char line[ LINE_LENGTH + 1 ];
     while ( fscanf( input, "%s", line ) == 1 ) {
         int current = largestProduct( line, count );
