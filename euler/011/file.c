68add1f94103e337ed0613c1a82b55caec2f59de
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-05-21 02:17:13 +0300

    Close to solving 11

--- euler/011/file.c
+++ euler/011/file.c
@@ -14,7 +14,7 @@ static int getLargestHorizontal( int grid[][ 20 ] )
     for ( int i = 0; i < 20; i++ ) {
         int *row = grid[ i ];
         int rowLargest = -1;
-        for ( int j = 0; j < 20 - 3; j++ ) {
+        for ( int j = 0; j < 20 - 4; j++ ) {
             int current = 1;
             for ( int k = 0; k < 4; k++ ) {
                 current *= row[ j + k ];
@@ -23,22 +23,66 @@ static int getLargestHorizontal( int grid[][ 20 ] )
         }
         largest = max( largest, rowLargest );
     }
+
+    return largest;
 }
 
-// static int getLargestVertical( int grid[][ 20 ] )
-// {
+static int getLargestVertical( int grid[][ 20 ] )
+{
+    int largest = -1;
 
-// }
+    for ( int j = 0; j < 20; j++ ) {
+        int columnLargest = -1;
+        for ( int i = 0; i < 20 - 3; i++ ) {
+            int current = 1;
+            for ( int k = 0; k < 4; k++ ) {
+                current *= grid[ i + k ][ j ];
+            }
+            columnLargest = max( columnLargest, current );
+        }
+        largest = max( largest, columnLargest );
+    }
 
-// static int getLargestDiagonal1( int grid[][ 20 ] )
-// {
+    return largest;
+}
 
-// }
+static int getLargestDiagonal1( int grid[][ 20 ] )
+{
+    int largest = -1;
 
-// static int getLargestDiagonal2( int grid[][ 20 ] )
-// {
+    for ( int i = 0; i < 20 - 4; i++ ) {
+        int waveLargest = -1;
+        for ( int j = 0; j < 20 - 4; j++ ){
+            int current = 1;
+            for ( int k = 0; k < 4; k++ ) {
+                current *= grid[ i + k ][ j + k ]; 
+            }
+            waveLargest = max( waveLargest, current );
+        }
+        largest = max( largest, waveLargest );
+    }
 
-// }
+    return largest;
+}
+
+static int getLargestDiagonal2( int grid[][ 20 ] )
+{
+    int largest = -1;
+
+    for ( int i = 0; i < 20 - 4; i++ ) {
+        int waveLargest = -1;
+        for ( int j = 20 - 1; j >= 4 - 1; j-- ) {
+            int current = 1;
+            for ( int k = 0; k < 4; k++ ) {
+                current *= grid[ i - k ][ j - k ];
+            }
+            waveLargest = max( waveLargest, current );
+        }
+        largest = max( largest, waveLargest );
+    }
+
+    return largest;
+}
 
 int main()
 {
@@ -64,9 +108,20 @@ int main()
     // }
 
     int largestHorizontal = getLargestHorizontal( grid );
-    // int largestVertical = getLargestVertical( grid );
-    // int largestDiagonal1 = getLargestDiagonal1( grid );
-    // int largestDiagonal2 = getLargestDiagonal2( grid );
+    int largestVertical = getLargestVertical( grid );
+    int largestDiagonal1 = getLargestDiagonal1( grid );
+    int largestDiagonal2 = getLargestDiagonal2( grid );
+
+    printf( "Largest horizontal: %d\n", largestHorizontal );
+    printf( "Largest vertical: %d\n", largestVertical );
+    int max12 = max( largestHorizontal, largestVertical );
+    printf( "max of 1-2: %d\n", max12 );
+    printf( "Largest diagonal1: %d\n", largestDiagonal1 );
+    printf( "Largest diagonal2: %d\n", largestDiagonal2 );
+    int max34 = max( largestDiagonal1, largestDiagonal2 );
+    printf( "max of 3-4: %d\n", max34 );
+    int maxAll = max( max12, max34 );
+    printf( "max of all: %d\n", maxAll );
 
     return EXIT_SUCCESS;
 }
