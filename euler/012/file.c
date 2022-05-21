f373349a062e238cc51a31feda2e770b37ccfe43
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-05-22 02:09:21 +0300

    Start 12

---
+++ euler/012/file.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// Solely in vim, this time
+int main()
+{
+    char str[] = "Hello World!";
+    printf( "%s\n", str );
+
+    return EXIT_SUCCESS;
+}
