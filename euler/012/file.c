c0b80cd3f31a33e32751b7d5500ebe1fd0951271
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-05-22 02:39:42 +0300

    Infinite loop

--- euler/012/file.c
+++ euler/012/file.c
@@ -2,11 +2,33 @@
 #include <stdio.h>
 #include <math.h>
 
+// Works for 2 and higher
+static int hasDivisors( int divisors )
+{
+  int current = 0;
+  for ( int i = 1;; i++ ) {
+    current += i;
+    int temp = current;
+
+    for ( int j = 1; temp != 1; j++ ) {
+      int count = 0;
+
+      if ( temp % j == 0 ) {
+        temp /= j;
+        count += 2;
+      }
+
+      if ( count >= divisors ) {
+        return current;
+      }
+    }
+  }    
+}
+
 // Solely in vim, this time
 int main()
 {
-    char str[] = "Hello World!";
-    printf( "%s\n", str );
+  printf( "Least number with %d divisors is %d", 5, hasDivisors( 5 ) );  
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
