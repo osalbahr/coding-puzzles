04cecae159f4782df056b92be125764bcef5cba9
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-05-22 03:37:45 +0300

    Works but slow

--- euler/012/file.c
+++ euler/012/file.c
@@ -8,6 +8,15 @@ static int hasDivisors( int divisors )
   int current = 0;
   for ( int i = 1;; i++ ) {
     current += i;
+    // if ( i % 100 == 0 )
+    //   printf( "current: %d\n", current );
+
+    // Detect overflow
+    if ( current < 0 ) {
+      printf( "%d%d\n", current, i );
+      exit( 1 );
+    }
+
     int count = 0;
     for ( int j = 1; j <= current; j++ ) {
       if ( current % j == 0 ) {
@@ -22,9 +31,9 @@ static int hasDivisors( int divisors )
 }
 
 // Solely in vim, this time
-int main()
+int main( int argc, char *argv[] )
 {
-  for ( int i = 2; i <= 10; i++ ) {
+  for ( int i = 1; i <= atoi(argv[ 1 ]); i++ ) {
     printf( "Least number with %d divisors is %d\n", i, hasDivisors( i ) );  
   }
 
