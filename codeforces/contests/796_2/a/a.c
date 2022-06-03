7ce765378ddfbb21d7bc78b6977a8002ae12e825
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-06-04 00:36:16 +0300

    Handled and/xor separately as in editorial

--- codeforces/contests/796_2/a/a.c
+++ codeforces/contests/796_2/a/a.c
@@ -3,6 +3,7 @@
 int getMinXor( n )
 {
   for ( int i = 1;; i <<= 1 ) {
+    // printf("A");
     if ( ( n ^ i ) > 0 ) {
       return i;
     }
@@ -13,8 +14,10 @@ int getMin( int n )
 {
   int result = 0;
   for ( int i = 1;; i <<= 1 ) {
-    if ( ( n & i ) > 0 && ( n ^ i ) > 0 ) {
+    // printf("B");
+    if ( ( n & i ) > 0 /* && ( n ^ i ) > 0 */ ) { // Why infinite loop?
       result += i;
+      break;
     }
   }
 
