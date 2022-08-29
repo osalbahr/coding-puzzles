f0671a187a552e76fe4c235c38772d4b21730eff
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-08-29 19:06:17 -0400

    Attempted closingtheloop

---
+++ kattis/problems/closingtheloop/closingtheloop.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#include <stdlib.h>
+
+#define REPORT( X ) printf( "%s = %d\n", #X, (X) )
+
+using namespace std;
+
+int solve()
+{
+  int s;
+  cin >> s;
+  vector<int> b;
+  vector<int> r;
+
+  for ( int i = 0; i < s; i++ ) {
+    string str;
+    cin >> str;
+    int size = str.size();
+    char color = str[ size - 1 ];
+    str = str.substr( 0, size - 1 );
+    int current = stoi( str );
+
+    if ( color == 'B' ) {
+      b.push_back( current );
+    } else {
+      r.push_back( current );
+    }
+  }
+
+  if ( b.size() == 0 || r.size() == 0 ) {
+    return 0;
+  } 
+
+  sort( b.begin(), b.end() );
+  sort( r.begin(), r.end() );
+
+  int ib = b.size() - 1, ir = r.size() - 1;
+  int result = 0;
+  char startColor;
+  if ( b[ ib ] > r[ ir ] ) {
+    result += b[ ib-- ];
+    startColor = 'B';
+  } else {
+    result += r[ ir-- ];
+    startColor = 'R';
+  }
+
+  char currentColor = startColor;
+  int knots = 1;
+  for (; ;) {
+    if ( currentColor == 'R' ) {
+      result += b[ ib-- ];
+      knots++;
+      currentColor = 'B';
+      if ( ir == -1 ) break;
+    } else {
+      result += r[ ir-- ];
+      knots++;
+      currentColor = 'R';
+      if ( ib == -1 ) break;
+    }
+  }
+
+  return result - knots;
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+    
+  for ( int i = 1; i <= t; i++ ) {
+    cout << "Case #" << i << ": " << solve() << endl;
+  }
+}
