f37f938b1c1ac1e67996e650b8fc5d096bfb2fe2
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-09-21 16:20:33 -0400

    Better sol

--- job/lucid/lucid.cpp
+++ job/lucid/lucid.cpp
@@ -2,18 +2,33 @@
 
 using namespace std;
 
-int getTransformedLength(string word, int t) {
-  while ( t-- ) {
-    for ( int i = 0; i < word.size(); i++ ) {
-      char ch = word[ i ];
-      if ( ch == 'z' ) {
-        word.replace( i, 1, "ab" );
-      } else {
-        word[ i ] = ch + 1;
-      }
-    }
+inline int add( int a, int b )
+{
+  int result = a + b;
+  if ( result >= 1000000007 )
+    return result % 1000000007;
+  
+  return result;
+}
+// Gives how long "ch" will be
+// after t transformations
+int getLength( char ch, int t )
+{
+  int distToZ = 'z' - ch;
+  if ( t <= distToZ ) {
+      return 1;
   }
-  return word.size();
+
+  t -= distToZ + 1;
+  return add( getLength( 'a', t ), getLength( 'b', t ) );
+ }
+
+int getTransformedLength(string word, int t) {
+  int result = 0;
+  for ( int i = 0; i < word.size(); i++ ) {
+    result = add( result, getLength( word[ i ], t ) );
+  }
+  return result;
 }
 
 int main()
